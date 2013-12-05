3d69d38179475c625f8f0b6ed3677931ed09d2ea
Leon Lin <leonxlin@gmail.com>
2013-12-04 19:42:04 -0500

    Implemented Cell.neighbors()

--- map.js
+++ map.js
@@ -1,7 +1,11 @@
 
 var Map = {
     settings: {
-        SIZE: 10
+        SIZE: 10,
+        START_R: 9,
+        START_C: 4,
+        NEIGHBOR_R_DELTAS: [-1, 0, 1, 0],
+        NEIGHBOR_C_DELTAS: [0, 1, 0, -1]
     },
     init: function() {
         this.$map = $('#map');
@@ -21,14 +25,20 @@ var Map = {
 
         // fill with words
         this.cells.forEach(function(cell) {
-            cell.setWord(Map.getWord(1));
+            cell.setWord(Map.getWord(5));
         });
     },
     getWord: function(difficulty) {
-        return "hullo";
+        nWords = Words["" + difficulty].length;
+        idx = Math.floor(Math.random()*nWords);
+        return Words["" + difficulty][idx];
     },
     cell: function(r, c) {
         return this.cells[this._cellIndex(r, c)];
+    },
+    validRC: function(r, c) {
+        max = this.settings.SIZE;
+        return 0 <= r && r < max && 0 <= c && c < max;
     },
     _cellIndex: function(r, c) {
         return r*this.settings.SIZE + c;
@@ -39,7 +49,7 @@ function Cell($cell, r, c) {
     this.$cell = $cell;
     this.r = r;
     this.c = c;
-    this.word = null;
+    this.word = "";
 }
 
 Cell.prototype.setWord = function(word) {
@@ -63,4 +73,20 @@ Cell.prototype.showAsTyped = function(typed) {
                     + "<span class='untyped'>"
                     + this.word.substring(typed.length)
                     + "</span>");
+};
+
+Cell.prototype.neighbors = function() {
+    var dr = Map.settings.NEIGHBOR_R_DELTAS,
+        dc = Map.settings.NEIGHBOR_C_DELTAS,
+        r = 0,
+        c = 0,
+        neighbors = [];
+    for (var i=0; i<dr.length; i++) {
+        r = this.r + dr[i];
+        c = this.c + dc[i];
+        if (Map.validRC(r, c)) {
+            neighbors.push(Map.cell(r, c));
+        }
+    }
+    return neighbors;
 };
